cbc5ab9bd9c714af1b2bd494c59a5c3c6b99c73e
Dennis Francis <dennis.francis@collabora.co.uk>
2018-10-29 13:54:47 +0530

    stubbed mta methods

---
+++ mta.go
@@ -0,0 +1,43 @@
+package mta
+
+import (
+	"fmt"
+)
+
+func NewMultiTypeArray(initSize int64, types []SingleTypeArray) (*MultiTypeArray, error) {
+	ret := &MultiTypeArray{}
+	var err error = nil
+	for _, arr := range types {
+		err = ret.AddType(arr)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return ret, nil
+}
+
+func (self *MultiTypeArray) AddType(arr SingleTypeArray) error {
+	typ := arr.Type()
+	if _, present := self.registeredTypes[typ]; present {
+		return fmt.Errorf("SingleTypeArray of Type = %d already registered", typ)
+	}
+	self.registeredTypes[typ] = arr
+	return nil
+}
+
+func (self *MultiTypeArray) Insert(elements interface{}, typeCode, size, pos int64) error {
+	return nil
+}
+
+func (self *MultiTypeArray) Delete(start, end int64) error {
+	return nil
+}
+
+func (self *MultiTypeArray) Copy(srcStart, srcEnd, destStart int64) error {
+	return nil
+}
+
+func (self *MultiTypeArray) Move(srcStart, srcEnd, destStart int64) error {
+	return nil
+}
