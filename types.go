a00167e3d31ae1231bb2d365f387b42d90e72b90
Dennis Francis <dennis.francis@collabora.co.uk>
2018-10-30 12:55:04 +0530

    redesign SingleTypeArray interface and add documentation

--- types.go
+++ types.go
@@ -1,13 +1,29 @@
 package mta
 
 type SingleTypeArray interface {
+
+	// Returns type code of the underlying type
 	Type() int64
+
+	// Returns newly created SingleTypeArray of the same type with "size" number of elements
 	New(size int64) SingleTypeArray
+
+	// Returns the number of elements in the array
 	Size() int64
+
+	// Returns a slice [start, end] of the bare array. Both indices are inclusive.
+	GetSlice(start, end int64) (elements interface{}, err error)
+
+	// Inserts the slice "elements" of length "size" at "pos" position in the array.
+	// The underlying type of "elements" must match that of the array.
 	Insert(elements interface{}, size, pos int64) error
+
+	// Deletes the elements in the array from index "start" to the index "end", both inclusive.
 	Delete(start, end int64) error
-	Copy(source SingleTypeArray, srcStart, srcEnd, destStart int64) (extendedSize int64, err error)
-	Move(source SingleTypeArray, srcStart, srcEnd, destStart int64) error
+
+	// Replaces the content of the array that starts from "pos" to "pos + size - 1" (both inclusive)
+	// with the first "size" elements of "elements".
+	Replace(elements interface{}, size, pos int64) error
 }
 
 type blockType struct {
