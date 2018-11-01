25d42e4350eb49d1675a60c823f21b09f0a961b7
Dennis Francis <dennis.francis@collabora.co.uk>
2018-11-01 13:04:35 +0530

    make a copy of locals.go to stas package

---
+++ stas/locals.go
@@ -0,0 +1,36 @@
+package stas
+
+import (
+	"fmt"
+)
+
+func validateIndex(idx, size int64) error {
+	if size < 0 {
+		return fmt.Errorf("size < 0")
+	}
+	if idx < 0 {
+		return fmt.Errorf("index is negative")
+	}
+	if idx >= size {
+		return fmt.Errorf("index is out of bounds")
+	}
+	return nil
+}
+
+func validateBeginEndIndices(beg, end, size int64) (err error) {
+	err = nil
+	errStart := validateIndex(beg, size)
+	if errStart != nil {
+		err = errStart
+		return
+	}
+	errEnd := validateIndex(end, size)
+	if errEnd != nil {
+		err = errEnd
+		return
+	}
+	if end < beg {
+		err = fmt.Errorf("end < beg")
+	}
+	return
+}
